f661a04094d8b5f6cab70a760f3523729df6893e
Alec Hayden <haydena@oregonstate.edu>
2020-09-25 17:38:34 -0700

    Simple error catching and refactorings

--- modules/createUniqueLink.js
+++ modules/createUniqueLink.js
@@ -1,7 +1,7 @@
 // Node modules
 const crypto = require("crypto");
 
-// Simple function to create email-specific link to send to user
+// Simple function to create email-specific link with random string
 module.exports = recipientEmail => {
   return "http://localhost:8080/appointment_form?email=" + recipientEmail + "&id=" + crypto.randomBytes(20).toString('hex');
 }
--- server.js
+++ server.js
@@ -10,10 +10,14 @@ const sendEmail = require('./modules/sendEmail');
 const createUniqueLink = require('./modules/createUniqueLink');
 
 
-var pseudoDatabase = [];    // Holds records of emails sent for tracking
-const submitEmailForm = fs.readFileSync("./templates/website_templates/submit_email_form.html");                   // Homepage
-const appointmentConfirmation = fs.readFileSync("./templates/website_templates/appointment_form_submitted.html");  // Appointment confirmation page
-const thankYou = fs.readFileSync("./templates/website_templates/thank_you.html");                                  // Final thank you page
+var pseudoDatabase = [];    // Holds user records of emails sent and forms filed
+const submitEmailForm = fs.readFileSync("./templates/website_templates/submit_email_form.html");                                                 // Homepage
+const activationEmailTemplate = fs.readFileSync(path.join(__dirname, 'templates/email_templates', 'activation_email.html'));                     // Unique Link email
+const appointmentConfirmation = fs.readFileSync("./templates/website_templates/appointment_form_submitted.html");                                // Appointment confirmation page
+const companyConfirmationEmailTemplate = fs.readFileSync(path.join(__dirname, 'templates/email_templates', 'company_confirmation_email.html'));  // Company appointment email
+const userConfirmationEmailTemplate = fs.readFileSync(path.join(__dirname, 'templates/email_templates', 'user_confirmation_email.html'));        // User appointment email
+const alreadySubmittedFormTemplate = fs.readFileSync(path.join(__dirname, 'templates/website_templates', 'already_submitted_form.html'));        // Already submitted page
+const thankYou = fs.readFileSync("./templates/website_templates/thank_you.html");                                                                // Final thank you page
 
 // Create the web server
 http.createServer(async function (req, res) {
@@ -30,9 +34,7 @@ http.createServer(async function (req, res) {
   if (q.pathname == "/send_email") {
     try {
       const uniqueLink = createUniqueLink(q.query.email);        // Create email-specific link to send to user
-      const activationEmailTemplate = fs.readFileSync(path.join(__dirname, 'templates/email_templates', 'activation_email.html'));
       const result = await sendEmail(q.query.email, 'Schedule Your Car Repair!', activationEmailTemplate, { uniqueLink });   // Send link to user's email
-
       pseudoDatabase.push({ email: q.query.email,   // Add record to pseudoDatabase
                             link: uniqueLink,
                             submitted: false,
@@ -63,106 +65,122 @@ http.createServer(async function (req, res) {
 
   // User clicks their email-specific link and is directed here
   if (q.pathname == "/appointment_form") {
-    // Check if user has already submitted form at their unique link
-    let record = pseudoDatabase.find(r => r.link === 'http://localhost:8080/appointment_form?email=' + q.query.email + '&id=' + q.query.id);
-    if (record['submitted'] === true) {
-      const data = {
-        "firstName": record['firstName'],
-        "lastName": record['lastName'],
-        "phoneNumber": record['phoneNumber'],
-        "vehicleYear": record['vehicleYear'],
-        "vehicleMake": record['vehicleMake'],
-        "vehicleModel": record['vehicleModel'],
-        "repairRequired": record['repairRequired'],
-        "date": record['date'],
-        "time": record['time']
-      };
-      const alreadySubmittedFormTemplate = fs.readFileSync(path.join(__dirname, 'templates/website_templates', 'already_submitted_form.html'));
-      let body = alreadySubmittedFormTemplate.toString();
+    try {
+      // Check if user has already submitted form at their unique link
+      let record = pseudoDatabase.find(r => r.link === 'http://localhost:8080/appointment_form?email=' + q.query.email + '&id=' + q.query.id);
+      if (record['submitted'] === true) {
+        let body = alreadySubmittedFormTemplate.toString();
+        body = body.toString().replace('%user_email%', q.query.email);
+        body = body.replace('%unique_link%', q.query.id);
+        const data = {
+          "firstName": record['firstName'],
+          "lastName": record['lastName'],
+          "phoneNumber": record['phoneNumber'],
+          "vehicleYear": record['vehicleYear'],
+          "vehicleMake": record['vehicleMake'],
+          "vehicleModel": record['vehicleModel'],
+          "repairRequired": record['repairRequired'],
+          "date": record['date'],
+          "time": record['time']
+        };
+        Object.keys(data).forEach(key => {
+          body = body.split('%'+key+'%').join(data[key]);
+        });
+
+        // Return a page saying they have already submitted the form
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(body);
+        return;
+      }
+
+      // Keep track of users email, uniqueLink, and ip address via hidden form input
+      let body = await fsp.readFile("./templates/website_templates/submit_appointment_form.html");
       body = body.toString().replace('%user_email%', q.query.email);
       body = body.replace('%unique_link%', q.query.id);
-      Object.keys(data).forEach(key => {
-        body = body.split('%'+key+'%').join(data[key]);
-      });
-      // Return a page saying they have already submitted the form
+      body = body.replace('%ip_address%', req.connection.remoteAddress);
+
+      // Display appointment form
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.end(body);
-
-      return;
-    }
-
-    // Keep track of users email, uniqueLink, and ip address via hidden form input
-    let body = await fsp.readFile("./templates/website_templates/submit_appointment_form.html");
-    body = body.toString().replace('%user_email%', q.query.email);
-    body = body.replace('%unique_link%', q.query.id);
-    body = body.replace('%ip_address%', req.connection.remoteAddress);
-
-    // Display appointment form
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(body);
-    return;
   }
+  catch (error) {
+    res.writeHead(500, {'Content-Type': 'text/html'});
+    res.end(error.message);
+  }
+  return;
+}
 
   // After user submits appointment form they're redirected here
   if (q.pathname == "/appointment_confirmation") {
-    // Add appointment details to existing record in the pseudoDatabase
-    let record = pseudoDatabase.find(r => r.link === 'http://localhost:8080/appointment_form?email=' + q.query.email + '&id=' + q.query.id);
-    record['submitted'] = true;
-    record['firstName'] = q.query.fname;
-    record['lastName'] = q.query.lname;
-    record['phoneNumber'] = q.query.phone;
-    record['vehicleYear'] = q.query.year;
-    record['vehicleMake'] = q.query.make;
-    record['vehicleModel'] = q.query.model;
-    record['repairRequired'] = q.query.repair;
-    record['date'] = q.query.date;
-    record['time'] = q.query.time;
+    try {
+      // Add appointment details to existing record in the pseudoDatabase
+      let record = pseudoDatabase.find(r => r.link === 'http://localhost:8080/appointment_form?email=' + q.query.email + '&id=' + q.query.id);
+      record['submitted'] = true;
+      record['firstName'] = q.query.fname;
+      record['lastName'] = q.query.lname;
+      record['phoneNumber'] = q.query.phone;
+      record['vehicleYear'] = q.query.year;
+      record['vehicleMake'] = q.query.make;
+      record['vehicleModel'] = q.query.model;
+      record['repairRequired'] = q.query.repair;
+      record['date'] = q.query.date;
+      record['time'] = q.query.time;
 
-    // Send confirmation email to user
-    const userConfirmationEmailTemplate = fs.readFileSync(path.join(__dirname, 'templates/email_templates', 'user_confirmation_email.html'));
-    const userResult = await sendEmail(q.query.email, 'Repair Appointment Confirmed!', userConfirmationEmailTemplate,
-    {
-      "firstName": q.query.fname,
-      "lastName": q.query.lname,
-      "phoneNumber": q.query.phone,
-      "vehicleYear": q.query.year,
-      "vehicleMake": q.query.make,
-      "vehicleModel": q.query.model,
-      "repairRequired": q.query.repair,
-      "date": q.query.date,
-      "time": q.query.time
-    });
+      // Send confirmation email to user
+      const userResult = await sendEmail(q.query.email, 'Repair Appointment Confirmed!', userConfirmationEmailTemplate,
+      {
+        "firstName": q.query.fname,
+        "lastName": q.query.lname,
+        "phoneNumber": q.query.phone,
+        "vehicleYear": q.query.year,
+        "vehicleMake": q.query.make,
+        "vehicleModel": q.query.model,
+        "repairRequired": q.query.repair,
+        "date": q.query.date,
+        "time": q.query.time
+      });
 
-    // Send appointment info to repairs@example.com
-    const companyConfirmationEmailTemplate = fs.readFileSync(path.join(__dirname, 'templates/email_templates', 'company_confirmation_email.html'));
-    const companyResult = await sendEmail(q.query.email, 'Car Repair Appointment', companyConfirmationEmailTemplate,
-    {
-      "firstName": q.query.fname,
-      "lastName": q.query.lname,
-      "phoneNumber": q.query.phone,
-      "vehicleYear": q.query.year,
-      "vehicleMake": q.query.make,
-      "vehicleModel": q.query.model,
-      "repairRequired": q.query.repair,
-      "date": q.query.date,
-      "time": q.query.time,
-      "ipAddress": q.query.ip
-    });
+      // Send appointment info to repairs@example.com
+      const companyResult = await sendEmail(q.query.email, 'Car Repair Appointment', companyConfirmationEmailTemplate,
+      {
+        "firstName": q.query.fname,
+        "lastName": q.query.lname,
+        "phoneNumber": q.query.phone,
+        "vehicleYear": q.query.year,
+        "vehicleMake": q.query.make,
+        "vehicleModel": q.query.model,
+        "repairRequired": q.query.repair,
+        "date": q.query.date,
+        "time": q.query.time,
+        "ipAddress": q.query.ip
+      });
 
-    // Form submitted and emails sent succesfully, so let user know to check inbox
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(appointmentConfirmation);
+      // Form submitted and emails sent succesfully, so let user know to check inbox
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(appointmentConfirmation);
+    }
+    catch (error) {
+      res.writeHead(500, {'Content-Type': 'text/html'});
+      res.end(error.message);
+    }
     return;
   }
 
   // After user gives a rating they are taken to this thank you page
   if (q.pathname == "/thank_you") {
-    // Get users record from pseudoDatabase and update with their rating
-    let record = pseudoDatabase.find(r => r.link === 'http://localhost:8080/appointment_form?email=' + q.query.email + '&id=' + q.query.id);
-    record['rating'] = q.query.rating;
+    try {
+      // Get users record from pseudoDatabase and update with their rating
+      let record = pseudoDatabase.find(r => r.link === 'http://localhost:8080/appointment_form?email=' + q.query.email + '&id=' + q.query.id);
+      record['rating'] = q.query.rating;
 
-    // Display thank you page
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(thankYou);
+      // Display thank you page
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(thankYou);
+    }
+    catch (error) {
+      res.writeHead(500, {'Content-Type': 'text/html'});
+      res.end(error.message);
+    }
+    return;
   }
 }).listen(8080, () => console.log('server listening'));
